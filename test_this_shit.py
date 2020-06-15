28fbd4c70d91c5553a2fa2e8a912486478cac435
asgard125 <asgardkarpov@yandex.ru>
2020-06-15 15:01:16 +1000

    changed lazy type

--- test_this_shit.py
+++ test_this_shit.py
@@ -1,6 +1,7 @@
 from requests import get, post, delete, put
 import json
 from hashlib import md5
+import datetime
 
 
 def test(method, address, json_args):
@@ -14,25 +15,25 @@ def test(method, address, json_args):
         print(delete(address, json=json.loads(json_args)).json())
 
 
-local_address = 'http://127.0.0.1:8080/'
-server_address = 'https://smarttracker.herokuapp.com/'
-
+local_address = 'http://127.0.0.1:8080'
+server_address = 'https://smarttracker.herokuapp.com'
+API_KEY = 'x8gfV5Ujukhs_1'
 shit = '''{"name": "genadiy",
                  "login": "asgard3@ori.ru",
                  "password": "123"}'''
-shit1 = '''{"email": "asgard3@ori.ru",
+shit1 = '''{"login": "asgard3@ori.ru",
                  "password": "123"}'''
-shit2 = '''{"api_key": "gFBDVvJaOQ_1"}'''
-shit3 = '''{"api_key": "gFBDVvJaOQ_1", "user_id": "1", "habit_type": "all", "info_type": "detail"}'''
-habit_shit = '''{"description": "anal sex", "name": "chlen", "type": "public", "weekdays": "1, 0", "notify_time": "19:00", "api_key": "gFBDVvJaOQ_1"}'''
-test('post', f'{server_address}/register', shit)
+shit69 = '''{"api_key": "3FDdW_1", "info_type": "private"}'''
+shit2 = '''{"api_key": "3FDdW_1", "info_type": "public"}'''
+shit3 = '''{"api_key": "3FDdW_1", "user_id": "3", "habit_type": "all", "info_type": "detail"}'''
+habit_shit = '''{"description": "anal sex", "name": "chlen", "type": "public", "weekdays": "1, 0", "notify_time": "19:00", "api_key": "3FDdW_1", "muted": "True"}'''
+rating_shit = '''{"api_key": "3FDdW_1", "limit": "10", "offset": "1"}'''
+# test('post', f'{local_address}/register', shit)
 # test('get', f'{local_address}/login', shit1)
-# test('get', f'{local_address}/api/v1/users', shit2)
-# # test('get', f"{local_address}/api/v1/habit/1", shit2)
-# test('get', f"{local_address}/api/v1/habit/1", shit2)
+test('get', f'{local_address}/api/v1/user/0', shit2)
+test('delete', f"{local_address}/api/v1/habit/3", shit2)
 # test('post', f"{local_address}/api/v1/habits", habit_shit)
-# test('get', f"{local_address}/api/v1/habits", shit3)
-
+test('get', f"{local_address}/api/v1/user/0", shit69)
 
 # from apscheduler.schedulers.background import BackgroundScheduler
 #
@@ -55,3 +56,5 @@ test('post', f'{server_address}/register', shit)
 #         json = load(file)
 #         for key, value in json.items():
 #             environ[key] = value
+
+#    conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
