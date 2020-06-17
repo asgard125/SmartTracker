ddade1df9a5141ba74b24a8f409946e61c737ea3
asgard125 <asgardkarpov@yandex.ru>
2020-06-17 15:32:17 +1000

    added sorts for habits list

--- data/db_session.py
+++ data/db_session.py
@@ -22,7 +22,7 @@ def global_init(db_file):
     conn_str = DATABASE_URL
     print(f"Подключение к базе данных по адресу {conn_str}")
 
-    engine = sa.create_engine(conn_str, echo=False, pool_class=NullPool)
+    engine = sa.create_engine(conn_str, echo=False, poolclass=NullPool)
     __factory = orm.sessionmaker(bind=engine)
 
     from . import __all_models
